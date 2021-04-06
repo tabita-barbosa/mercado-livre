ace88d2705c5a9b466c89f0664ef151813da10fe
tabita <tabita.barbosa8@gmail.com>
2021-04-06 17:57:39 -0300

    primeiro commit

---
+++ src/test/java/br/com/itau/ml/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.ml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
